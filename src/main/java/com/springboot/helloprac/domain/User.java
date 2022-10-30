cfd81da43307498266276c1aea786a2fe56603d3
이수진 <slrkajsep337@naver.com>
2022-10-31 00:51:16 +0900

    user 생성

---
+++ src/main/java/com/springboot/helloprac/domain/User.java
@@ -0,0 +1,32 @@
+package com.springboot.helloprac.domain;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String password;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+
+
+
+}
