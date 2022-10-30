bb7cdfa2a500a1e54098861569c85a797dc8aab4
이수진 <slrkajsep337@naver.com>
2022-10-31 00:51:01 +0900

    swagger-ui 실습

---
+++ src/main/java/com/springboot/helloprac/domain/dto/MemberDto.java
@@ -0,0 +1,25 @@
+package com.springboot.helloprac.domain.dto;
+
+public class MemberDto {
+
+
+    private String name;
+    private String email;
+    private String organization;
+
+
+    public MemberDto(String name, String email, String organization) {
+        this.name = name;
+        this.email = email;
+        this.organization = organization;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s", this.name, this.email, this.organization);
+
+    }
+
+
+
+}
