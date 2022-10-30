4b2699784fe3551b5a86568658fa23efad589196
이수진 <slrkajsep337@naver.com>
2022-10-31 00:48:32 +0900

    first commit

---
+++ src/main/java/com/springboot/helloprac/HelloPracApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.helloprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/helloprac/HelloPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.helloprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
