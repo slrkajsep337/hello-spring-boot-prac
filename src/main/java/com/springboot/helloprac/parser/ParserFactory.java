5c262759dbb632cb8f1dbad744d4c55f825361ee
이수진 <slrkajsep337@naver.com>
2022-10-31 16:04:05 +0900

    10만건 초과 데이터가 파싱되는지 test 통과

---
+++ src/main/java/com/springboot/helloprac/parser/ParserFactory.java
@@ -0,0 +1,14 @@
+package com.springboot.helloprac.parser;
+
+import com.springboot.helloprac.domain.Hospital;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ParserFactory {
+
+    @Bean
+    public ReadLineContext<Hospital> hospitalReadLineContext() {
+        return new ReadLineContext<Hospital>(new HospitalParser());
+    }
+ }
