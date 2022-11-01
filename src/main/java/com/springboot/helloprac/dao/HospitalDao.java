8510cbaa523f1e37deabfc60699d80f04a61c5b5
이수진 <slrkajsep337@naver.com>
2022-11-01 16:14:32 +0900

    주석추가

--- src/main/java/com/springboot/helloprac/dao/HospitalDao.java
+++ src/main/java/com/springboot/helloprac/dao/HospitalDao.java
@@ -2,6 +2,7 @@ package com.springboot.helloprac.dao;
 
 import com.springboot.helloprac.domain.Hospital;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component //SpringBootApplication에서 @Component가 달려있는 클래스를 Bean으로 등록한다
@@ -39,5 +40,36 @@ public class HospitalDao {
         String sql = "select count(id) from nation_wide_hospital;";
         return this.jt.queryForObject(sql, Integer.class);
         //queryForObject(a, b) :  a-sql문, b-반환받을 객체의 타입(e.g.) String- String.class)
+    }
+
+    public void deleteAll() {
+        this.jt.update("delete from nation_wide_hospital;");
+    }
+
+    RowMapper<Hospital> rowMapper = (rs, rowNum) -> {
+        Hospital hospital = new Hospital();
+        hospital.setId(rs.getInt("id"));
+        hospital.setOpenServiceName(rs.getString("open_service_name"));
+        hospital.setOpenLocalGovernmentCode(rs.getInt("open_local_government_code"));
+        hospital.setManagementNumber(rs.getString("management_number"));
+        hospital.setLicenseDate(rs.getTimestamp("license_date").toLocalDateTime());
+        hospital.setBusinessStatus(rs.getInt("business_status"));
+        hospital.setBusinessStatusCode(rs.getInt("business_status_code"));
+        hospital.setPhone(rs.getString("phone"));
+        hospital.setFullAddress(rs.getString("full_address"));
+        hospital.setRoadNameAddress(rs.getString("road_name_address"));
+        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setBusinessTypeName(rs.getString("business_type_name"));
+        hospital.setHealthcareProviderCount(rs.getInt("healthcare_provider_cnt"));
+        hospital.setPatientRoomCount(rs.getInt("patient_room_cnt"));
+        hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
+        hospital.setTotalAreaSize(rs.getFloat("total_area_size"));
+
+        return hospital;
+    };
+
+    public Hospital findById(int id) {
+        return this.jt.queryForObject("select * from nation_wide_hospital where id=?", rowMapper, id);
+        //queryForObject(a, b, c) :  a-sql문, b-반환받을 객체의 타입(e.g.) String- String.class), c- ?에 들어갈 값
     }
 }
