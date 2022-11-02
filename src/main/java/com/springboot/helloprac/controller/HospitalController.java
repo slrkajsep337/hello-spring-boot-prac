8dcf29d5efc5151c643375190606d3ce04da245f
이수진 <slrkajsep337@naver.com>
2022-11-02 13:16:17 +0900

    findById()에서 Optional 사용 추가

--- src/main/java/com/springboot/helloprac/controller/HospitalController.java
+++ src/main/java/com/springboot/helloprac/controller/HospitalController.java
@@ -5,8 +5,11 @@ import com.springboot.helloprac.dao.HospitalDao;
 import com.springboot.helloprac.domain.Hospital;
 import com.springboot.helloprac.domain.dto.HospitalRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v2")
@@ -28,7 +31,19 @@ public class HospitalController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Hospital> findById(@PathVariable int id) {
-        return ResponseEntity.ok().body(this.hd.findById(id));
+        Hospital hospital = hd.findById(id);
+        Optional<Hospital> opt = Optional.of(hospital);
+
+        //nullporinterexception을 처리할 때 -> Optional 이용
+        //Optioner - null이 올 수 있는 값을 감싸는 wrapper class ->따라서 NPE가 발생하지 않는다
+        //isEmpty로 값이 비었는지(null)인지 확인할 수 있다.
+        if (!opt.isEmpty()) {
+            return ResponseEntity.ok().body(hospital);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
+
+//            return ResponseEntity.ok().body(this.hd.findById(id));
+        }
     }
 
     @DeleteMapping("/hospital")
