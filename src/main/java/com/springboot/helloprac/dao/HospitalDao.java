ab0fd42a213cac62ced14c9010c9618f2e5082b6
이수진 <slrkajsep337@naver.com>
2022-11-02 14:26:31 +0900

    오타 수정

--- src/main/java/com/springboot/helloprac/dao/HospitalDao.java
+++ src/main/java/com/springboot/helloprac/dao/HospitalDao.java
@@ -15,7 +15,9 @@ public class HospitalDao {
     }
 
     public int add(Hospital hospital) {
-        return this.jt.update("insert into users(id, user, password) value (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        return this.jt.update("INSERT INTO `hospital`.`nation_wide_hospital` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, " +
+                "`phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_cnt`, `patient_room_cnt`, `total_number_of_beds`, `total_area_size`) \n" +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);",
                 hospital.getId(),
                 hospital.getOpenServiceName(),
                 hospital.getOpenLocalGovernmentCode(),
